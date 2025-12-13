a033971b0f5bde21a6999fa2fb50adcb2f121ded
Raj Verma <rajkumargrd56@gmail.com>
2025-12-13 21:24:39 +0530

    vercel.json is added

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ const chalk = require("chalk");
 const videoRouter = require("./routes/videoCreator");
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true
 }))
 
@@ -50,3 +50,6 @@ initializeConnection();
 //         })
 //     })
 //     .catch(err => console.log("Error: " + err));
+
+// Export for Vercel serverless
+module.exports = app;
