e5a1fda92aa9479c3661355965ff08c0416963dd
Raj Verma <rajkumargrd56@gmail.com>
2025-12-15 15:46:11 +0530

    problemSolved Schema bug fixed

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -36,18 +36,27 @@ const userSchema = new Schema({
         enum: ["admin", "user"],        // only these value will be set
         default: "user"
     },
+    // problemSolved: {
+    //     type: [{
+    //         type: Schema.Types.ObjectId,        // unique solved stored
+    //         ref: "problem"
+    //     }],
+    // }
+
     problemSolved: {
         type: [{
-            type: Schema.Types.ObjectId,        // unique solved stored
+            type: Schema.Types.ObjectId,
             ref: "problem"
         }],
+        default: []
     }
+
 }, { timestamps: true });
 
 // it will run post(after the findOneByIDAndDelete) in the last to delete everyting related this to this userId
 userSchema.post("findOneAndDelete", async function (userInfo) {
     if (userInfo) {
-        await mongoose.model("submission").deleteMany({ userId: userInfo_id });
+        await mongoose.model("submission").deleteMany({ userId: userInfo._id });
     }
 })
 
